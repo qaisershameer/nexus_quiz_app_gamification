1d2cb5ee190a582559e4b477d7c94427ec9f3579
Qaiser Shameer <naeem.malik009@gmail.com>
2025-02-27 17:03:24 +0500

    Feb27 KBS

--- lib/screens/question_screen.dart
+++ lib/screens/question_screen.dart
@@ -8,29 +8,123 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [Colors.green, Colors.yellow],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Question Screen - Animation Effects'),
+        backgroundColor: Colors.orange.shade200,
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Title
-          Text('Guess the Animal Voice',
-            style: GoogleFonts.lato(
-              color: Colors.white,
-              fontSize: 24,
-              decoration: TextDecoration.none,
-            ),
+      body: Container(
+        margin: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(8.0),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.green, Colors.yellow],
           ),
-          const SizedBox(height: 16.0,),
-        ],
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 3),
+                    curve: Curves.fastEaseInToSlowEaseOut,
+                    height: isExpanded ? 200 : 100,
+                    width: isExpanded ? 150 : 50,
+                    color: isExpanded ? Colors.green : Colors.deepPurple,
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 3),
+                    curve: Curves.easeInOut,
+                    height: isExpanded ? 200 : 100,
+                    width: isExpanded ? 150 : 50,
+                    color: isExpanded ? Colors.yellow : Colors.teal,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 3),
+                    curve: Curves.bounceInOut,
+                    height: isExpanded ? 200 : 100,
+                    width: isExpanded ? 150 : 50,
+                    color: isExpanded ? Colors.red : Colors.black,
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 3),
+                    curve: Curves.fastOutSlowIn,
+                    height: isExpanded ? 200 : 100,
+                    width: isExpanded ? 150 : 50,
+                    color: isExpanded ? Colors.cyan : Colors.pink,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 3),
+                    curve: Curves.decelerate,
+                    height: isExpanded ? 200 : 100,
+                    width: isExpanded ? 150 : 50,
+                    color: isExpanded
+                        ? Colors.pinkAccent
+                        : Colors.deepOrangeAccent,
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  AnimatedContainer(
+                    duration: const Duration(seconds: 3),
+                    curve: Curves.easeInOutCubicEmphasized,
+                    height: isExpanded ? 200 : 100,
+                    width: isExpanded ? 150 : 50,
+                    color: isExpanded ? Colors.deepOrange : Colors.teal,
+                  ),
+                ],
+              ),
+              FilledButton(
+                onPressed: () {
+                  setState(() {
+                    isExpanded = !isExpanded;
+                    // print('Button Clicked ${isExpanded}');
+                  });
+                },
+                child: Text(isExpanded ? 'Shrink' : 'Expanded'),
+              ),
+              Text(
+                'Animated Controller Assignment',
+                style: GoogleFonts.lato(
+                  color: Colors.black,
+                  fontSize: isExpanded ? 16 : 24,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
